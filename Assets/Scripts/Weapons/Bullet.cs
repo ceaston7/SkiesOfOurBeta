3f173851ff626f81c12e4683b8e3c1bed78973ed
ceaston7 <ceaston7@hotmail.com>
2022-01-01 20:50:40 -0500

    HUD, ammo counters, missile timer
    - HUD
    - ammo counters
    - missile reload timer
    - health counter (non-functioning)

--- Assets/Scripts/Weapons/Bullet.cs
+++ Assets/Scripts/Weapons/Bullet.cs
@@ -4,28 +4,27 @@ using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
-    float despawnTimer;
+    float despawnTimer = 5.0f;
     public Transform parentTransform;
 
     void Start()
     {
-        transform.localScale = new Vector3(0.2f, 0.2f, 0.2f);
+        transform.localScale = new Vector3(0.1f, 0.1f, 0.1f);
 
         var rigidbody = gameObject.AddComponent<Rigidbody>();
         rigidbody.useGravity = false;
-        rigidbody.velocity = parentTransform.forward * 20;
+        rigidbody.velocity = parentTransform.GetComponent<Rigidbody>().velocity + parentTransform.forward * 20;
         gameObject.transform.position = parentTransform.position + parentTransform.forward * parentTransform.localScale.z;
 
-        despawnTimer = 0;
         StartCoroutine(DespawnTimer());
     }
 
     IEnumerator DespawnTimer()
     {
-        while (despawnTimer < 5.0f)
+        while (despawnTimer > 0f)
         {
             //Debug.Log("Despawn timer: " + despawnTimer);
-            despawnTimer += Time.deltaTime;
+            despawnTimer -= Time.deltaTime;
             yield return null;
         }
 
