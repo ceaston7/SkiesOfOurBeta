fd3c1b2330ef9dbfcbc03c894d90656923041dc4
ceaston7 <ceaston7@hotmail.com>
2022-02-25 18:46:56 -0500

    player vfx

---
+++ Assets/Scripts/PlayerVFX.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerVFX : MonoBehaviour
+{
+    //Thrusters
+    public GameObject thrustFore;
+    public GameObject thrustStarboardFore;
+    public GameObject thrustStarboardStern;
+    public GameObject thrustPortFore;
+    public GameObject thrustPortStern;
+    public GameObject thrustStern;
+
+    public void Thrust(Thruster thruster, bool show){
+        switch(thruster){
+            case Thruster.fore:
+                thrustFore.SetActive(show);
+                break;
+            case Thruster.starboardFore:
+                thrustStarboardFore.SetActive(show);
+                break;
+            case Thruster.starboardStern:
+                thrustStarboardStern.SetActive(show);
+                break;
+            case Thruster.portFore:
+                thrustPortFore.SetActive(show);
+                break;
+            case Thruster.portStern:
+                thrustPortStern.SetActive(show);
+                break;
+            case Thruster.stern:
+                thrustStern.SetActive(show);
+                break;
+            default:
+                break;
+        }
+    }
+
+    public enum Thruster{
+        fore = 0,
+        starboardFore,
+        starboardStern,
+        portFore,
+        portStern,
+        stern
+    }
+}
